17f1c4f44660769cb26fc15687c6a3b987ae4dea
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-02-08 22:04:49 +0200

    Add files via upload

---
+++ flagify.bat
@@ -0,0 +1,28 @@
+@echo off
+title wow u suck
+
+rem Create a new directory named "diarrhea"
+mkdir diarrhea
+
+rem Open the newly created folder
+explorer diarrhea
+
+rem Open the specified Roblox user's profile in the default browser
+start https://www.roblox.com/users/profile?userId=
+
+rem Create a README.txt file with the word "fart" inside
+echo fart > "diarrhea\README.txt"
+
+rem Create 5 batch files in the "diarrhea" folder
+for /l %%n in (1,1,5) do (
+    (
+        echo @echo off
+        echo title droolin_rn
+        echo for /l %%i in (1, 1, 1^) do ^(
+        echo    timeout /t 10 /nobreak ^>nul
+        echo ^)
+    ) > "diarrhea\file%%n.bat"
+)
+
+echo lol i diarrhea'd a folder to ur pc
+pause
