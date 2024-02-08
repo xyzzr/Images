10313d5d9f0c31f654f0978da54a3fe285240b66
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-02-08 22:07:24 +0200

    Update flagify.bat

--- flagify.bat
+++ flagify.bat
@@ -1,19 +1,11 @@
 @echo off
 title wow u suck
-
-rem Create a new directory named "diarrhea"
 mkdir diarrhea
-
-rem Open the newly created folder
 explorer diarrhea
-
-rem Open the specified Roblox user's profile in the default browser
 start https://www.roblox.com/users/profile?userId=
-
-rem Create a README.txt file with the word "fart" inside
 echo fart > "diarrhea\README.txt"
 
-rem Create 5 batch files in the "diarrhea" folder
+
 for /l %%n in (1,1,5) do (
     (
         echo @echo off
